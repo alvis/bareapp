751b280a953a1f34450deffbcc2b7062ee46f4f6
Alvis HT Tang <alvis@hilbert.space>
2020-07-23 17:17:43 +0100

    feat: set to skip the onboarding page once it's dismissed

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -23,7 +23,11 @@ import {
 } from '@ionic/react';
 import React, { useContext } from 'react';
 
+import PreferenceContext from '../../preference';
+
 export default function Onboarding(): ReturnType<React.FC> {
+  const preference = useContext(PreferenceContext);
+
   return (
     <IonPage>
       <IonContent fullscreen class="ion-padding">
@@ -31,7 +35,10 @@ export default function Onboarding(): ReturnType<React.FC> {
       </IonContent>
       <IonFooter className="ion-no-border">
         <IonToolbar>
-          <IonButton expand="block" routerLink="/home">
+          <IonButton
+            expand="block"
+            routerLink="/home"
+            onClick={async () => preference.update({ showOnboarding: false })}>
             OK
           </IonButton>
         </IonToolbar>
