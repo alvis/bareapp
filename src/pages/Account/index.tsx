afdf04cdb959f968ed0f90c73d9ad143cd9268f7
Alvis HT Tang <alvis@hilbert.space>
2020-08-05 15:18:49 +0100

    feat: add an account page

---
+++ src/pages/Account/index.tsx
@@ -0,0 +1,64 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Account Management
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2020 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+import {
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonIcon,
+  IonItem,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonLabel,
+  IonList,
+  IonPage,
+} from '@ionic/react';
+import React from 'react';
+
+// import './Account.css';
+
+import Private from '../../components/Private';
+
+import { mailOpen } from 'ionicons/icons';
+
+export default function Account(): ReturnType<React.FC> {
+  return (
+    <Private>
+      {({ identity, logout }) => (
+        <IonPage>
+          <IonContent>
+            <IonCard>
+              <img src={identity.picture} alt="profile" />
+              <IonCardHeader>
+                <IonCardSubtitle>Welcome</IonCardSubtitle>
+                <IonCardTitle>{identity.name}</IonCardTitle>
+              </IonCardHeader>
+              <IonList>
+                <IonItem>
+                  <IonIcon icon={mailOpen} slot="start" />
+                  <IonLabel>{identity.email}</IonLabel>
+                </IonItem>
+              </IonList>
+            </IonCard>
+            <IonButton onClick={logout} color="danger" expand="block">
+              Logout
+            </IonButton>
+          </IonContent>
+        </IonPage>
+      )}
+    </Private>
+  );
+}
